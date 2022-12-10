9248de9b545ec5c3100f5d1be44107e4f5d743f8
Muhammad Bilal <50591961+bilalthereal@users.noreply.github.com>
2022-12-10 20:38:45 +0500

    Add Starting Project

---
+++ quotes/pipelines.py
@@ -0,0 +1,40 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+# Used to store data in database.
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import sqlite3 
+
+class QuotesPipeline:
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn=sqlite3.connect("myquotes.db")
+        self.curr=self.conn.cursor()
+    
+    def create_table(self):
+        self.curr.execute("""Drop table if exists quotes_db""")
+        self.curr.execute("""
+    create table quotes_db(
+        title text,
+        author text,
+        tags text
+    )""")
+
+    def process_item(self, item, spider):
+        self.store_db(item)
+        return item
+
+    def store_db(self,item):
+        self.curr.execute("""insert into quotes_db values (?,?,?)""",(
+            item['title'][0],
+            item['author'][0],
+            item['tags'][0]
+        ))
+        self.conn.commit()
